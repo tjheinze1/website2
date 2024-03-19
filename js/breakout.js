5261848dbb481bc7e35ca7e0f171e60e5169ead5
tjheinze1 <142804675+tjheinze1@users.noreply.github.com>
2024-03-19 22:12:47 +0000

    Tue, Mar 19, 2024, 3:12 PM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -1,7 +1,12 @@
 rulesBtn = document.getElementById('rules-btn')
 rules = document.getElementById('rules')
-
+closeBtn = document.getElementById('close-btn')
 
 rulesBtn.addEventListener('click', () => {
     rules.classList.add('show')
+})
+
+
+closeBtn.addEventListener('click', () => {
+    rules.classList.remove('show')
 })
